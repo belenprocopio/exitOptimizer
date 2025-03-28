343447337773048f1681b354f7bb56abe720bf88
belenprocopio <u1973781@campus.udg.edu>
2025-03-28 15:18:31 +0100

    Add files via upload

---
+++ exitos/rootfs/OS_lab.py
@@ -0,0 +1,163 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from BESS_lab import BESS_lab as Bess
+
+class OS_lab():
+    
+    def __init__(self, 
+                 elec_prices=[0.133, 0.126, 0.128, 0.141, 0.147, 0.148, 0.155, 0.156, 0.158, 0.152, 0.147, 0.148, 0.144, 0.141, 0.139, 0.136, 0.134, 0.137, 0.143, 0.152, 0.157, 0.164, 0.159, 0.156],
+                 venta_electr = [0.054,0.054,0.054,0.054,0.054,0.054,0.054,0.054,0.054,0.054,0.054,0.054,0.054,0.054,0.054,0.054,0.054,0.054,0.054,0.054,0.054,0.054,0.054,0.054],
+                 building_dem=[1380.3333333333333,839.3076923076923,853.5833333333334,1623.8333333333333,846.2307692307693,918.9166666666666,1539.0,2118.714285714286,1464.0833333333333,438.0,417.2857142857143,438.5833333333333,408.5,1390.3846153846155,439.3333333333333,441.72727272727275,720.0666666666667,1643.6363636363637,2205.5833333333335,1630.0714285714287,1878.6363636363637,3408.8333333333335,2823.5,1976.6363636363637],
+                 solar_prod=[0.0,0.0,0.0,0.0,0.0,0.0,0.09090909090909091,26.5,74.5,205.54545454545453,285.14285714285717,481.25,711.8,506.38461538461536,379.0,181.36363636363637,135.53333333333333,26.272727272727273,0.08333333333333333,0.0,0.0,0.0,0.0,0.0],
+                 bat_inicial_master=91,
+                 bat_inicial_slave=91,
+                 debug=False
+                 ):
+        
+        """
+        Constructor de l'objecte, es passen per parametre tots els attributs necessaris per realitzar la simulacio i es guarden dins l'objecte.
+        """
+        #if debug will se prints
+        self.debug = debug
+        
+        # solar production
+        self.sp = np.array(solar_prod)
+        
+        # Consumptions edificis Tots sumats!
+        self.ec = np.array(building_dem)
+        
+        #preu de l'electricitat 
+        self.prices = np.array(elec_prices)
+        
+        #preus de venta d'electricitat
+        self.prices_venta = np.array(venta_electr)
+        
+        #preparem les bateries
+        self.Bess_master = Bess(bat_inicial_master, debug=self.debug)
+        self.Bess_slave = Bess(bat_inicial_slave, debug=self.debug)
+        #estat inicial bateries
+        self.bat_ini_master = bat_inicial_master
+        self.bat_ini_slave = bat_inicial_slave
+        
+        #algorisme solver
+        self.alg='SA' # 'PSO' = particle swarm; 'SA' = simulated anealing; 'DE' = Diferential Evolution; 'least_squares' = least_squares;
+        
+       
+    def cost(self,x):
+        """
+        Funcio de cost completa on s'optimitza totes les variables possibles.
+        """
+        ### BESS - Bateria electrica controlem que fa per percentatge
+        #x[0] fins a x[23]  --> Es el mode de operacio de la bateria.
+        x = list(np.around(np.array(x),0))
+        
+        powers_1=[]
+        powers_2=[]
+        m=[]
+        num_bat=2
+        lim_bat = num_bat*24 #Limite donde empiezan los datos de la siguiente batería.
+        for i in range(0,lim_bat):
+            #Traduim de la particula al metode desitjat.
+            if x[i] == 0:
+                m='off'
+            if x[i] == 1:
+                m='discharge'
+            if x[i] == 2:
+                m='charge'
+            
+            if i in range(0,24):
+                powers_1.append( self.Bess_master.control(method=m,load = self.ec[i], production = self.sp[i]))
+            elif i in range(24,lim_bat):
+                powers_2.append( self.Bess_slave.control(method=m,load = self.ec[i-24], production = self.sp[i-24]))
+        
+        #simulem bateria.
+        kwh_Bess_1, SoC_Bess_1 = self.Bess_master.simula(powers_1[0:])
+        kwh_Bess_2, SoC_Bess_2 = self.Bess_slave.simula(powers_2[0:])
+        
+        #calculem que gastem /injectem a xarxa
+        valans_energetic = self.ec - self.sp  + kwh_Bess_1 + kwh_Bess_2
+        
+        injectat =valans_energetic.copy()
+        injectat[injectat>0]=0 #treiem positius
+        injectat=abs(injectat) #valor absolut
+        
+        consumit = valans_energetic.copy()
+        consumit[consumit<0]=0
+        
+        #comptabilitzem els kw dins la bateria. A preu de venda mig diari. els inicials negatius els finals positius.
+        bats_1 = (SoC_Bess_1[-1] - self.bat_ini_master) * self.Bess_master.bat_max * self.prices_venta.mean()
+        bats_2 = (SoC_Bess_2[-1] - self.bat_ini_slave) * self.Bess_slave.bat_max * self.prices_venta.mean()
+        
+        #funcio de cost
+        preu = sum(consumit * self.prices) - sum(injectat * self.prices_venta) + bats_1 + bats_2       
+        return preu
+    
+    
+    def optimize(self):
+        """
+        Funcio que engega la optimitzacio corresponent. i retorna la millor comanda que s'ha trobat per retornar a l'ESB 
+        """
+        
+        if self.alg=='PSO':
+            import pyswarms as ps
+            particules=24# Create bounds
+            max_bound = [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]
+            min_bound = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+            bounds = (min_bound, max_bound)
+            
+            # Initialize swarm
+            options = {'c1': 2, 'c2': 2, 'w':0.4}
+            
+            # Call instance of PSO with bounds argument
+            optimizer = ps.single.GlobalBestPSO(n_particles=10, dimensions=particules, options=options, bounds=bounds)
+            def f(x):
+                x = list(np.around(np.array(x),0))
+                nparticles=x.shape[0]
+                preus_solucions=[self.cost(x[i]) for i in range(nparticles)]
+                return np.array(preus_solucions)
+            cost, x = optimizer.optimize(f, iters=10000)
+            x=np.array(x).round()
+            
+        elif self.alg=='SA':
+            from scipy.optimize import dual_annealing
+            max_bound = [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]
+            min_bound = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+            res=dual_annealing(self.cost, bounds=list(zip(min_bound,max_bound)), no_local_search = False)
+            x=res.x
+            x=np.array(res.x).round()
+            
+        elif self.alg=='DE':
+            from scipy.optimize import differential_evolution
+            max_bound = [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]
+            min_bound = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+            
+            def f(x):
+                x = list(np.around(np.array(x),0))
+                return self.cost(np.array(x).round())
+            res=differential_evolution(f, bounds=list(zip(min_bound,max_bound)))
+            x=np.array(res.x).round()
+                       
+        elif self.alg=='least_squares':
+            from scipy.optimize import least_squares
+            max_bound = [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]
+            min_bound = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+            
+            res=least_squares(self.cost,x0=np.array(max_bound)/2, bounds=(min_bound,max_bound))
+            x=res.x
+        
+        #Posem be el retorn, repetint les variables que calgui segons index
+        BESS = []
+        m =[]
+        num_bat=2
+        for i in range(0,24*num_bat):
+            #Traduim de la particula al metode desitjat.
+            if x[i] == 0:
+                m='off'
+            if x[i] == 1:
+                m='discharge'
+            if x[i] == 2:
+                m='charge'
+            BESS.append(m)
+        
+        
+        return np.array(BESS)
---
+++ exitos/rootfs/test_OS.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 18 14:55:52 2025
+
+@author: Belen
+"""
+import pandas as pd
+print("Leyendo datos")
+df_potencia_ct = pd.read_csv("datos\potencia_ct.csv", delimiter=',')
+df_solar1 = pd.read_csv("datos\paneles_master.csv", delimiter=',')
+df_solar2 = pd.read_csv("datos\paneles_slave.csv", delimiter=',')
+
+df_potencia_ct["last_changed"] = pd.to_datetime(df_potencia_ct["last_changed"])
+df_solar1["last_changed"] = pd.to_datetime(df_solar1["last_changed"])
+df_solar2["last_changed"] = pd.to_datetime(df_solar2["last_changed"])
+
+
+
+df_potencia_ct_resampled = df_potencia_ct.set_index("last_changed").resample("1h").first().reset_index()
+df_solar1_resampled = df_solar1.set_index("last_changed").resample("1h").first().reset_index().fillna(0)
+df_solar2_resampled = df_solar2.set_index("last_changed").resample("1h").first().reset_index().fillna(0)
+
+df_potencia_ct_resampled["state"] = pd.to_numeric(df_potencia_ct_resampled["state"],errors='coerce')
+df_solar1_resampled["state"] = pd.to_numeric(df_solar1_resampled["state"],errors='coerce')
+df_solar2_resampled["state"] = pd.to_numeric(df_solar2_resampled["state"],errors='coerce')
+
+
+import OS_lab as OS_lab
+"""
+Loads = [ 592.45634747, 592.49635485, 591.85913229, 587.32300037, 586.73134253, 658.20264316, 764.18414501, 892.49415179,1059.20503953,1139.24205145,1047.32752325, 911.3889703 , 830.37283932, 810.47591246, 725.17024209, 655.69050001, 633.23086019, 656.17916226, 674.2013454 , 613.44323685, 630.28952584, 623.30421259, 622.07467228, 615.39167492]
+PV=[5.94394677e+00,1.24269512e+00,9.34323393e-02,9.85697932e-02,4.45894808e-01,1.50230271e+02,7.28816702e+02,1.38984333e+03,1.98963365e+03,2.41026367e+03,2.53855949e+03,2.54364465e+03,1.89310506e+03,1.97221394e+03,1.47734965e+03,1.33669594e+03,4.51399602e+02,1.49279818e+02,3.94424950e+01,2.60363683e+01,1.23473593e+00,8.13592035e-02,2.45826099e-01,4.26342606e-01]
+bat = 0.5
+"""
+
+Loads = df_potencia_ct_resampled["state"].to_list()
+PV = df_solar1_resampled["state"].to_list() + df_solar2_resampled["state"].to_list()
+bat_master = 0.28
+bat_slave = 0.26
+
+
+"""
+Loads: consumo del edificio (kW)
+PV: producción solar (kW)
+bat: SoC inicial de la bateria
+"""
+max_len = 24
+if len(Loads) > max_len:
+    Loads = Loads[:max_len]
+if len(PV) > max_len:
+    PV = PV[:max_len]
+while len(PV) < len(Loads):
+    PV.append(0)
+
+Loads = [-x for x in Loads] #Cambio de signo
+#W a kW
+Loads = [x/1000 for x in Loads]
+PV = [x/1000 for x in PV]
+
+
+optimizer = OS_lab.OS_lab(building_dem=Loads,solar_prod=PV,bat_inicial_master=bat_master,bat_inicial_slave=bat_slave)
+
+print("Optimizando")
+BESS = optimizer.optimize()
+print("Optimizacion Finalizada")
+
+#Calculem la flexivilitat de la solucio trobada.
+#### Bateria
+import BESS_lab as BESS_lab
+
+SoC = bat_master 
+bateria_master = BESS_lab.BESS_lab(SoC,debug=False) #SoC inicial en tant per 1
+SoC = bat_slave 
+bateria_slave = BESS_lab.BESS_lab(SoC,debug=False) #SoC inicial en tant per 1
+
+#Calculem el power min i max que podrem posar /treure de la bateria segons el metode de control seleccionat i forcasts
+c_master=[]
+c_slave=[]
+for i in range(0,24):
+    c_master.append(bateria_master.control(method=BESS[i],load = Loads[i], production = PV[i]))
+    c_slave.append(bateria_master.control(method=BESS[i+24],load = Loads[i], production = PV[i]))
+    #positivo: potencia de carga; negativo: potencia de descarga
+
+print("Simulando Bateria 1")
+#donat el control Fem la simulacio.
+kwh_master, SoC_master = bateria_master.simula(c_master)
+print("Simulando Bateria 2")
+kwh_slave, SoC_slave = bateria_slave.simula(c_slave)
+#kwh negativo: extraido de la red; positivo: inyectado
+print("Simulacion Finalizada")
+
+#donada la simulacio calculem flexivilitat que tindrem
+# dt_BESS_master, dt_plus_BESS_master, dt_minus_BESS_master = bateria_master.flex(kwh_master, SoC_master)
+# dt_BESS_slave, dt_plus_BESS_slave, dt_minus_BESS_slave = bateria_slave.flex(kwh_slave, SoC_slave)
+
+##visualitzem (positiu bateria consumeix de xarxa - Negatiu injecta)
+import matplotlib.pyplot as plt
+# plt.figure()
+# plt.plot(dt_BESS, label='Consum Bateria',  color='k')
+# plt.plot(dt_minus_BESS+dt_BESS, label='Consum minim',  color='b', linestyle='-.')
+# plt.plot(dt_plus_BESS+dt_BESS, label='Consum maxim',   color='r', linestyle='-.')
+# plt.legend()
+# plt.show()
+plt.figure()
+plt.plot(kwh_master, label='Consum Bateria Master',  color='k')
+plt.plot(kwh_slave, label='Consum Bateria Slave',  color='b', linestyle='-.')
+plt.legend()
+plt.show()
