7a9e6e20fbbf10113a717cd1c9b997b4a27ffdf2
belenprocopio <u1973781@campus.udg.edu>
2025-03-31 16:40:30 +0200

    Update post_state.py

--- exitos/rootfs/post_state.py
+++ exitos/rootfs/post_state.py
@@ -1,8 +1,17 @@
 from requests import post
 
-url = "http://localhost:8123/api/states/sensor.kitchen_temperature"
-headers = {"Authorization": "Bearer TOKEN", "content-type": "application/json"}
-data = {"state": "25", "attributes": {"unit_of_measurement": "°C"}}
+def post_state(id_sensor,state,unit_of_measurement):
+  #token api interna.
+  token = os.environ.get('SUPERVISOR_TOKEN')
+  
+  #preparem la url de la api corresponent a la id que volem canviar
+  url = "http://localhost:8123/api/states/"+id_sensor
+  headers = {
+    "Authorization": "Bearer " +token, 
+    "content-type": "application/json",
+  }
+  
+  data = {"state": state, "attributes": {"unit_of_measurement": unit_of_measurement}}
 
-response = post(url, headers=headers, json=data)
-print(response.text)
+  response = post(url, headers=headers, json=data)
+  print(response.text)
