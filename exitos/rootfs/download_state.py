08905b0170ee5dbcc6f0119be6e5924c36ff7dca
belenprocopio <u1973781@campus.udg.edu>
2025-03-31 14:56:15 +0200

    Update download_state.py

--- exitos/rootfs/download_state.py
+++ exitos/rootfs/download_state.py
@@ -2,37 +2,35 @@ from requests import get
 import pandas as pd
 import os
 
-#token api interna.
-token = os.environ.get('SUPERVISOR_TOKEN')
-
-#especifiquem Id que volem
-id_sensor = "sensor.sunvec_potencia_paneles_master"
+def download_state(id_sensor):
+  #token api interna.
+  token = os.environ.get('SUPERVISOR_TOKEN')
+  
+  #especifiquem Id que volem
+  # id_sensor = "sensor.sunvec_potencia_paneles_master"
              
 
-#preparem la url de la api corresponent a la id i periode que volem descarregar
-url = "http://supervisor/core/api/states/"+id_sensor
+  #preparem la url de la api corresponent a la id i periode que volem descarregar
+  url = "http://supervisor/core/api/states/"+id_sensor
 
-headers = {
-    "Authorization": "Bearer "+token,
-    "content-type": "application/json",
-}
+  headers = {
+      "Authorization": "Bearer "+token,
+      "content-type": "application/json",
+  }
 
-#Llegim les dades de la url
-response = get(url, headers=headers)
+  #Llegim les dades de la url
+  response = get(url, headers=headers)
 
-if response.status_code == 200:
-    try:
-        sensor_data_historic = pd.json_normalize(response.json())
-    except ValueError as e:
-        print("Error parsing JSON: " + str(e))
-elif response.status_code == 500:
-    print("Server error (500): Internal server error at sensor ", id_sensor)
-    sensor_data_historic = pd.DataFrame()
-else:
-    print("Request failed with status code: ", response.status_code)
-    sensor_data_historic = pd.DataFrame()
-    
-print("Sensor:")
-print(sensor_data_historic)
-print("State:")
-print(sensor_data_historic['state'][0])
+  if response.status_code == 200:
+      try:
+          sensor_data_historic = pd.json_normalize(response.json())
+      except ValueError as e:
+          print("Error parsing JSON: " + str(e))
+  elif response.status_code == 500:
+      print("Server error (500): Internal server error at sensor ", id_sensor)
+      sensor_data_historic = pd.DataFrame()
+  else:
+      print("Request failed with status code: ", response.status_code)
+      sensor_data_historic = pd.DataFrame()
+
+  return(sensor_data_historic['state'][0])
