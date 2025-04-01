1cb7e59a2181029100b9cf23f08c7a8cb3ab6175
belenprocopio <u1973781@campus.udg.edu>
2025-04-01 17:07:56 +0200

    Update price.py

--- exitos/rootfs/price.py
+++ exitos/rootfs/price.py
@@ -1,6 +1,7 @@
 from datetime import datetime 
 from datetime import timedelta
 import requests
+
  # Calculate the date for tomorrow
 tomorrow = datetime.today() + timedelta(1)
 tomorrow_str = tomorrow.strftime('%Y%m%d')
@@ -14,3 +15,20 @@ if response.status_code != 200:
     today = datetime.today().strftime('%Y%m%d')
     url = f"https://www.omie.es/es/file-download?parents%5B0%5D=marginalpdbc&filename=marginalpdbc_{today}.1"
     response = requests.get(url)
+    
+
+
+if response.status_code == 200:
+    lines = response.text.split("\r\n")
+    elec_prices = []
+    for line in lines:
+        parts = line.split(";")  # Separar columnas  
+        if len(parts) > 5 and int(parts[0]) == tomorrow.year:
+            try:
+                price = float(parts[-2])
+                elec_prices.append(price)
+            except ValueError:
+                pass
+    print(elec_prices)  # Lista final de precios
+else:
+    print("Error al obtener los datos de OMIE.")
