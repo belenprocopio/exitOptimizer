f844bde00fb12c57c8628e877d80a51a1ff426f7
belenprocopio <u1973781@campus.udg.edu>
2025-04-03 08:38:04 +0200

    Update test_OS.py

--- exitos/rootfs/test_OS.py
+++ exitos/rootfs/test_OS.py
@@ -16,6 +16,8 @@ Loads = [ 592.45634747, 592.49635485, 591.85913229, 587.32300037, 586.73134253, 
 PV = [5.94394677e+00,1.24269512e+00,9.34323393e-02,9.85697932e-02,4.45894808e-01,1.50230271e+02,7.28816702e+02,1.38984333e+03,1.98963365e+03,2.41026367e+03,2.53855949e+03,2.54364465e+03,1.89310506e+03,1.97221394e+03,1.47734965e+03,1.33669594e+03,4.51399602e+02,1.49279818e+02,3.94424950e+01,2.60363683e+01,1.23473593e+00,8.13592035e-02,2.45826099e-01,4.26342606e-01]
 
 print("Lectura de variables")
+sys.stdout.flush()
+
 bat_master = download_state.download_state("sensor.sunvec_capacidad_bateria_master")/100
 bat_slave = download_state.download_state("sensor.sunvec_capacidad_bateria_slave_1")/100
 print("Bateria Master:")
@@ -29,13 +31,16 @@ print(prices)
 
 print("Variables listas")
 sys.stdout.flush()
-time.sleep(1)
 
 optimizer = OS_lab.OS_lab(elec_prices=prices,building_dem=Loads,solar_prod=PV,bat_inicial_master=bat_master,bat_inicial_slave=bat_slave)
 
 print("Optimizando")
+sys.stdout.flush()
+
 BESS = optimizer.optimize()
+
 print("Optimizacion Finalizada")
+sys.stdout.flush()
 
 #Calculem la flexivilitat de la solucio trobada.
 #### Bateria
@@ -54,13 +59,14 @@ for i in range(0,24):
     c_slave.append(bateria_master.control(method=BESS[i+24],load = Loads[i], production = PV[i]))
     #positivo: potencia de carga; negativo: potencia de descarga
 
-print("Simulando Bateria 1")
+print("Simulando Baterias")
+sys.stdout.flush()
 #donat el control Fem la simulacio.
 kwh_master, SoC_master = bateria_master.simula(c_master)
-print("Simulando Bateria 2")
 kwh_slave, SoC_slave = bateria_slave.simula(c_slave)
 #kwh negativo: extraido de la red; positivo: inyectado
 print("Simulacion Finalizada")
+sys.stdout.flush()
 
 wh_master = [round(num*1000) for num in kwh_master]
 wh_slave = [round(num*1000) for num in kwh_slave]
@@ -68,6 +74,7 @@ SoC_master = [round(num*100) for num in SoC_master]
 SoC_slave = [round(num*100) for num in SoC_slave]
 
 print("Publicando Estados:")
+sys.stdout.flush()
 for i in range(0,24):
     post_state.post_state("input_text.modo_optimo_bateria_master",BESS[i])
     print("Control Master Valor ",i," Publicado: ", BESS[i])
@@ -81,14 +88,17 @@ for i in range(0,24):
     print("SoC Master Valor ",i," Publicado: ", SoC_master[i])
     post_state.post_state("input_number.prediccion_soc_bateria_slave",SoC_slave[i])
     print("SoC Slave Valor ",i," Publicado: ", SoC_slave[i])
+    sys.stdout.flush()
     time.sleep(3600)
 
 post_state.post_state("input_text.modo_optimo_bateria_master",'off')
 print("Estado Final Control Master Publicado: ", 'off')
 post_state.post_state("input_text.modo_optimo_bateria_slave",'off')
 print("Estado Final Control Slave Publicado: ", 'off')
+sys.stdout.flush()
 
 print("Todos los estados publicados")
+sys.stdout.flush()
 #donada la simulacio calculem flexivilitat que tindrem
 # dt_BESS_master, dt_plus_BESS_master, dt_minus_BESS_master = bateria_master.flex(kwh_master, SoC_master)
 # dt_BESS_slave, dt_plus_BESS_slave, dt_minus_BESS_slave = bateria_slave.flex(kwh_slave, SoC_slave)
